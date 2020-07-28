133a25876d2a8419314dcf5c529586dff0a4e062
Jungho Lee <wjdgh9577@naver.com>
2020-07-29 02:06:09 +0900

    2020.07.29

--- 자작/RunningGirl/Assets/Scripts/PlayerInput.cs
+++ 자작/RunningGirl/Assets/Scripts/PlayerInput.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    private PlayerPhysics playerPhysics;
+
+    private Vector2 pos;
+
     void Start()
     {
-        
+        playerPhysics = GetComponent<PlayerPhysics>();
+        StartCoroutine(Drag());
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            playerPhysics.Jump();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            playerPhysics.Dive();
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            playerPhysics.Fly();
+        }
+    }
+
+    IEnumerator Drag()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 nextPos = Input.mousePosition;
+                if (nextPos.y - pos.y > 10)
+                {
+                    playerPhysics.Jump();
+                }
+                pos = nextPos;
+                Debug.Log(pos);
+            }
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
     }
 }
--- 자작/RunningGirl/Assets/Scripts/PlayerPhysics.cs
+++ 자작/RunningGirl/Assets/Scripts/PlayerPhysics.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class PlayerPhysics : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+
+    [SerializeField]
+    private float jumpPower;
+
+    private bool canJump = true;
+    private bool canFly = false;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    public void Jump()
     {
-        
+        if (canJump)
+        {
+            rigidbody.velocity = Vector3.up * jumpPower;
+            canJump = false;
+            canFly = true;
+        }
+    }
+
+    public void Dive()
+    {
+        rigidbody.velocity = Vector3.down * jumpPower * 10;
+    }
+
+    public void Fly()
+    {
+        if (canFly)
+        {
+            rigidbody.velocity = Vector3.down * .1f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Field")
+        {
+            canJump = true;
+        }
     }
 }
