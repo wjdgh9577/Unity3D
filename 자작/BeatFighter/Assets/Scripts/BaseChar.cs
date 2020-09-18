8fa2bc7c4c6ecf0f1fd4bd3613ff409edba949b5
Jungho Lee <wjdgh9577@naver.com>
2020-09-19 05:35:16 +0900

    BeatFighter

---
+++ 자작/BeatFighter/Assets/Scripts/BaseChar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseChar : MonoBehaviour
+{
+    private Animator animator;
+
+    private BaseChar _target;
+    public BaseChar Target { get => _target; set => _target = value; }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Attack()
+    {
+        animator.Play("Attack");
+    }
+}
---
+++ 자작/BeatFighter/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    private float distance = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
