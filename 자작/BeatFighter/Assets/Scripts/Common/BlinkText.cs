74cf0b2b2908b3b2e56af017593b2eae7615266a
Jungho Lee <wjdgh9577@naver.com>
2020-10-16 22:32:34 +0900

    배경 터치 설명 추가

---
+++ 자작/BeatFighter/Assets/Scripts/Common/BlinkText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkText : MonoBehaviour
+{
+    [SerializeField]
+    private CustomText text;
+    private float alpha = 1;
+    private bool up = false;
+
+    private void Update()
+    {
+        if (up)
+        {
+            alpha += Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            if (alpha >= 1) up = false;
+        }
+        else
+        {
+            alpha -= Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            if (alpha <= 0) up = true;
+        }
+    }
+}
